a93c76ec7ca077350754b1f697f9557d86f1e85d
Konrad Strack <konrad@strack.pl>
2015-05-13 23:34:04 +0100

    highlight rows on hover

--- assets/js/application.js
+++ assets/js/application.js
@@ -4,8 +4,15 @@
 	app.controller('OrderController', ['$http',
 		function($http) {
 			var order = this;
-			order.episodes = [];
+			order.episodeTypes = [
+				['film', 'type-film'],
+				['SG1', 'type-sg1'],
+				['SGA', 'type-sga'],
+				['SGU', 'type-sgu'],
+				['KW', 'type-kw']
+			];
 
+			order.episodes = [];
 			$http.get('assets/data/episodes.json').success(function(data) {
 				order.episodes = data;
 			});
